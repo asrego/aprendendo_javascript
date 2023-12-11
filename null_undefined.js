46eb57ee4f15e7089bf442c683db61a8538c4e0e
Antoniel Rego <antonielsrego@gmail.com>
2023-12-11 19:05:12 -0300

    Valores Null vs Undefined

---
+++ null_undefined.js
@@ -0,0 +1,13 @@
+let input;
+
+if (input === undefined) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
+
+let input2 = null;
+let input3;
+
+console.log(input2); // null
+console.log(input3); // undefined
